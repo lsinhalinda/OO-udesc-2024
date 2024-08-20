76f9518c796c26afe00a2fd566a56b0dd6f613dd
Isa <souzaisaihd@gmail.com>
2024-08-19 21:48:21 -0300

    Classe OlaMundo e respectivo teste

---
+++ src/OlaMundo.java
@@ -0,0 +1,9 @@
+public class OlaMundo { String nome = "Udesc!";
+    int idade = 42;
+    void imprimirDados() {
+        System.out.println(nome + " - " + idade);
+    }
+    void imprimir(String nome, int idade) {
+        System.out.println(nome + " - " + idade);
+    }
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,9 @@
+public class TestaOlaMundo {
+    public static void main(String[] arg)
+    {
+        //cria uma classe a partir da classe 'OlaMundo'
+        OlaMundo ola = new OlaMundo();
+        ola.imprimirDados();
+        ola.imprimir("Fulando, 18");
+    }
+}
